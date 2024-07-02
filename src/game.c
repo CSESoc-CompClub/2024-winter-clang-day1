178059b3ddd7a7edc02968040e2abbab990b7c31
Vinayak Fialok <z5416144@ad.unsw.edu.au>
2024-07-02 20:15:57 +1000

    prog

--- src/game.c
+++ src/game.c
@@ -1,34 +1,74 @@
 #include "game.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdarg.h>
 
+// CONSTANTS ==========================
+int MAX_PLACES = 4;
+int MAX_PLACE_LENGTH = 12;
+
 // GAME FUNCTIONS =====================
 
-/**
- * build the player character based on user input
- */
 Player build_player() {
   Player player;
 
   slow_print("> What should we call you player?\n");
   char name[1024];
-  scanf("%s", name);
+  scanf("> %s", name);
   strcpy(player.name, name);
 
-  // TODO: the rest of building player
+  slow_print("> What is your age?\n");
+  scanf("> %d", &player.age);
+
+  slow_print("> What is your height?\n");
+  scanf("> %d", &player.height);
 
   return player;
 }
 
 void travelling(Player player) {
-  // TODO: implement this
+  char **places = malloc(MAX_PLACES * sizeof(char *));
+
+  for (int i = 0; i < MAX_PLACES; i++) {
+    places[i] = malloc((MAX_PLACE_LENGTH + 1) * sizeof(char));
+    char enter_place_str[1024];
+    sprintf(enter_place_str, "> Enter place %d: \n", i);
+    slow_print(enter_place_str);
+    scanf("> %s", places[i]);
+  }
+
+  for (int i = 0; i < MAX_PLACES; i++) {
+    choose_a_vehicle(player, places[i]);
+  }
 }
 
-char* choose_a_vehicle() {
-  // TODO: implement this
-  return NULL;
+void choose_a_vehicle(Player player, char *environment) {
+  char str[1024];
+
+  sprintf(str, "> You have arrive at %s.\n", environment);
+  slow_print(str);
+  slow_print("> Select a vehicle for your travels.\n");
+  slow_print("> - (h) Hoverboard\n");
+  slow_print("> - (q) Quantum High Speed Bike\n");
+  slow_print("> - (n) Nano Diffusion Jetpack\n");
+
+  char selection;
+  scanf("> %c", &selection);
+
+  if (selection == 'h') {
+    strcpy(player.vehicle, "Hoverboard");
+  } else if (selection == 'q') {
+    strcpy(player.vehicle, "Quantum High Speed Bike");
+  } else if (selection == 'n') {
+    strcpy(player.vehicle, "Nano Diffusion Jetpack");
+  } else {
+    strcpy(player.vehicle, "Super Secret Skateboard");
+  }
+
+  sprintf(str, "> You have chosen %s for your travels in %s!\n", player.vehicle, environment);
+  slow_print("> Good choice agent, you've made it to the next location.\n");
 }
 
 // UTILITY FUNCTION
@@ -46,4 +86,3 @@ void slow_print(const char *format, ...) {
   }
   putchar('\n');
 }
-
--- src/main.c
+++ src/main.c
@@ -8,6 +8,13 @@ int main(void) {
       "> Welcome to CyberNEx Corp candidate, today is your first day on the "
       "job and your final test, please check the file linked to neurochip. "
       "It includes all the information and details of mission REDACTED.\n");
+  slow_print("> You've been task with a mission to take out REDACTED\n");
+  slow_print("> Please choose your assigned identity for the mission. With a "
+             "top-secret mission, please ensure your true identity is not "
+             "revealed.\n");
   Player player = build_player();
-  // TODO: fill in based on the spec
+  char player_info_string[1024];
+  sprintf(player_info_string, "> Understood, welcome to the game %s. We've recorded your height as %d and age as %d.\n", player.name, player.height, player.age);
+  slow_print(player_info_string);
+  travelling(player);
 }
