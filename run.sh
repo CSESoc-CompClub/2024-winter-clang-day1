69175aad8e6778c8a529561dd10a2e25a2087947
Ashwin <ashwintheagarajan@gmail.com>
2024-07-09 19:43:54 +1000

    slow_print, run.sh changes

--- run.sh
+++ run.sh
@@ -3,12 +3,13 @@
 # Get tmp file name
 out_file=$(mktemp -q "./out.XXXXXX")
 
+# Remove
+trap 'rm -f "$out_file"' EXIT 
+
 # Compile main.c
 gcc src/main.c src/game.c -o "$out_file"
 
 # Run
 "./$out_file"
 
-# Remove
-rm "$out_file"
 
